8d14d0c763846c2d12cbe13cf4be43ac2ccc9001
eya-ghazouani <81834173+eya-ghazouani@users.noreply.github.com>
2021-12-31 23:09:26 +0100

    create get reservations api

--- frontend/angular/backend/app.js
+++ frontend/angular/backend/app.js
@@ -2,8 +2,58 @@ const express = require ('express');
 
 const app= express();
 
-app.use((req, res)=>{
-res.json('Hello world');
+app.get('/api/reservations',(req, res)=>{
+   
+    const reservations = [
+        {    
+            id : "1",
+            clientname :"aya",
+            datearrivee :"10-04-2000",
+            nbrnuits : 4,
+            nbrenfants : 3,
+            nbradultes : 2,
+            nbrchambres : 4,
+        },
+        {    
+            id :"2",
+            clientname :"aya",
+            datearrivee :"10-04-2000",
+            nbrnuits : 4,
+            nbrenfants : 3,
+            nbradultes : 2,
+            nbrchambres : 4,
+        },
+        {
+            id :"3",
+            clientname :"aya",
+            datearrivee :"10-04-2000",
+            nbrnuits : 4,
+            nbrenfants : 3,
+            nbradultes : 2,
+            nbrchambres : 4,
+        },
+        {
+            id :"4",
+            clientname :"aya",
+            datearrivee :"10-04-2000",
+            nbrnuits : 4,
+            nbrenfants : 3,
+            nbradultes : 2,
+            nbrchambres : 4,
+        },
+        {   
+             id :"5",
+            clientname :"aya",
+            datearrivee :"10-04-2000",
+            nbrnuits : 4,
+            nbrenfants : 3,
+            nbradultes : 2,
+            nbrchambres : 4,
+        },
+    
+    ];
+
+    res.json(reservations);
 });
 
 module.exports =app;
